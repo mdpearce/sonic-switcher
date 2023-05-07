548262bdefb232eb4c501efeb2773c7c7ecaf8d8
Michael Pearce <mdpearce@gmail.com>
2023-05-07 15:13:46 +1000

    Convert gradle to kotlin DSL

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,11 +6,14 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    @Suppress("UnstableApiUsage")
     repositories {
         google()
         mavenCentral()
     }
 }
 rootProject.name = "Sonic Switcher"
-include ':app'
+include(":app")
