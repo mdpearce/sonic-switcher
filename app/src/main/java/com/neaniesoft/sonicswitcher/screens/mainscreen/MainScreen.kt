b3779270d55ecfc9b8a08291513ce45218e217d0
Michael Pearce <mdpearce@gmail.com>
2023-05-15 19:06:47 +1000

    Add button to open file chooser

--- app/src/main/java/com/neaniesoft/sonicswitcher/screens/mainscreen/MainScreen.kt
+++ app/src/main/java/com/neaniesoft/sonicswitcher/screens/mainscreen/MainScreen.kt
@@ -1,15 +1,22 @@
 package com.neaniesoft.sonicswitcher.screens.mainscreen
 
+import android.media.MediaExtractor
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.neaniesoft.sonicswitcher.converter.PcmDecoderImpl
 
 @Composable
 fun MainScreen(viewModel: MainScreenViewModel = viewModel()) {
@@ -18,15 +25,25 @@ fun MainScreen(viewModel: MainScreenViewModel = viewModel()) {
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        Button(
-            onClick = { viewModel.onConvertClicked() },
-            modifier = Modifier.align(Alignment.BottomCenter)
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
         ) {
-            Text(text = "Convert")
+            Button(onClick = {}) {
+                Text(text = "Choose file")
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            Button(
+                onClick = { viewModel.onConvertClicked() }
+            ) {
+                Text(text = "Convert")
+            }
         }
     }
 }
 
-// @Preview
-// @Composable
-// fun PreviewMainScreen() = MainScreen() { MainScreenViewModel() }
+@Preview
+@Composable
+fun PreviewMainScreen() = MainScreen(MainScreenViewModel(PcmDecoderImpl { MediaExtractor() }))
