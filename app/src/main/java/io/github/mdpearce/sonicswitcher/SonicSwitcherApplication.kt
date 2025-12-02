c5daf3ba57d7ead8178baba76605ef9530efe8d8
Micaela Pearce <mdpearce@gmail.com>
2025-12-02 23:13:30 +1100

    Enable Crashlytics initialization for production crash reporting

--- app/src/main/java/io/github/mdpearce/sonicswitcher/SonicSwitcherApplication.kt
+++ app/src/main/java/io/github/mdpearce/sonicswitcher/SonicSwitcherApplication.kt
@@ -1,7 +1,16 @@
 package io.github.mdpearce.sonicswitcher
 
 import android.app.Application
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class SonicSwitcherApplication : Application()
+class SonicSwitcherApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Enable Crashlytics collection
+        // In debug builds, you might want to disable it: BuildConfig.DEBUG
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true)
+    }
+}
